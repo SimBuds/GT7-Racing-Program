8ffced0c4a9eb0e9a0271934efaee67d6e816160
Casey Hsu <Casey-Hsu@outlook.com>
2023-03-31 16:54:15 -0400

    Meow Push

--- Manager.py
+++ Manager.py
@@ -53,3 +53,53 @@ class Manager:
         Laps = self.Cursor.fetchall()
         for Lap in Laps:
             self.Laps.append(Lap)
+
+    def AddPlayer(self, Name, CarType, CarNumber):
+        self.Cursor.execute("INSERT INTO Players (Name, CarType, CarNumber) VALUES (?, ?, ?)", (Name, CarType, CarNumber))
+        self.Connection.commit()
+        self.Players.append((self.Cursor.lastrowid, Name, CarType, CarNumber))
+
+    def AddLap(self, Map, LapTime, PlayerId):
+        self.Cursor.execute("INSERT INTO Laps (Map, LapTime, PlayerId) VALUES (?, ?, ?)", (Map, LapTime, PlayerId))
+        self.Connection.commit()
+        self.Laps.append((self.Cursor.lastrowid, Map, LapTime, PlayerId))
+    
+    def RemovePlayer(self, Id):
+        self.Cursor.execute("DELETE FROM Players WHERE Id = ?", (Id))
+        self.Connection.commit()
+        for Player in self.Players:
+            if Player[0] == Id:
+                self.Players.remove(Player)
+                break
+
+    def RemoveLap(self, Id, Map, LapTime):
+        self.Cursor.execute("DELETE FROM Laps WHERE Id = ?", (Id))
+        self.Connection.commit()
+        for Lap in self.Laps:
+            if Lap[0] == Id:
+                self.Laps.remove(Lap)
+                break
+
+    def SearchLap(self, Map):
+        for Lap in self.Laps:
+            if Lap[1] == Map:
+                print(Lap)
+                
+    def SearchPlayer(self, Name):
+        for Player in self.Players:
+            if Player[1] == Name:
+                print(Player)
+
+    def DisplayLap(self):
+        for Lap in self.Laps:
+            print(Lap)
+    
+    def DisplayLap(self, Id):
+        for Lap in self.Laps:
+            if Lap[0] == Id:
+                print(Lap)
+
+    def DisplayPlayer(self, Id):
+        for Player in self.Players:
+            if Player[0] == Id:
+                print(Player)
